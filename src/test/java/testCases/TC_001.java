b0ceca9ba4b1ffd655d94f6c11b7e72ed93e762c
Raghavendra-Prasad-Gowda <36755879+Raghavendra-Prasad-Gowda@users.noreply.github.com>
2020-07-04 21:52:33 +0530

    Added TestNG Xml

--- src/test/java/testCases/TC_001.java
+++ src/test/java/testCases/TC_001.java
@@ -17,12 +17,20 @@ public class TC_001 extends BaseClass {
 
 		if (currentTemperatureValueFromWebsite < 19) {
 			homePageObjects.clickMoisturizers();
-			ArrayList<String> moisturizerNames = moisturizersObjects.getleastExpensiveAloe();
+			ArrayList<String> moisturizerNames1 = moisturizersObjects.getleastExpensiveAloe();
+			ArrayList<String> moisturizerNames2 = moisturizersObjects.getleastExpensiveAlmond();
+			
+			System.out.println(moisturizerNames1);
+			System.out.println(moisturizerNames2);
 
 			
 		} else if (currentTemperatureValueFromWebsite > 34) {
 			homePageObjects.clickSunscreens();
-
+			ArrayList<String> sunscreenNames1 = sunScreensObjects.getleastExpensiveSPF50();
+			ArrayList<String> sunscreenNames2 = sunScreensObjects.getleastExpensiveSPF30();
+			
+			System.out.println(sunscreenNames1);
+			System.out.println(sunscreenNames2);
 			
 
 		} else {
